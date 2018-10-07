2b6a847bfc7e234f34cbb9e09ad0914e4b358c22
Andrew Wong <andrew.wong.0101@gmail.com>
2018-10-08 00:14:31 +0800

    Add CIFAR MNIST random label

---
+++ MNIST_cnn_structure.py
@@ -0,0 +1,33 @@
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation,Flatten
+from keras.optimizers import SGD, Adam
+from keras.utils import np_utils
+from keras.callbacks import EarlyStopping
+import matplotlib.pyplot as plt
+from keras.layers import Conv2D, MaxPooling2D
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+num_classes = 10
+img_size = 28 # mnist size = 28*28
+
+def load_data():
+	# load mnist data
+	(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+	# preprocess data, let pixel between 0~1
+	x_train = x_train.reshape((x_train.shape[0],28,28,1))
+	x_train = x_train.astype('float32') / 255.
+
+	x_test = x_test.reshape((x_test.shape[0],28,28,1))
+	x_test = x_test.astype('float32') / 255.
+
+	y_train = np_utils.to_categorical(y_train, num_classes)
+	y_test = np_utils.to_categorical(y_test, num_classes)
+
+	return x_train, y_train, x_test, y_test
+
+x_train, y_train, x_test, y_test = load_data()
+
+print (y_train[0])
