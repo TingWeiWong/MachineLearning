a97e7e39b823b846c2c0476f9a1ed600f88d43d9
Andrew <andrew.wong.0101@gmail.com>
2020-10-29 21:07:43 +0800

    Add stump decision

---
+++ StumpDecision/stump.py
@@ -0,0 +1,19 @@
+import numpy as np
+import random
+
+# Global Parameters
+data_set_size = 2
+tau = 0
+
+# Placeholders
+Eout_minus_Ein = []
+
+# Flipping Function
+def flipping(original_array, data_set_size, tau):
+	"""
+	This function flips the y label by tau probability independently
+	- Input:
+		* original_array:
+		* 
+	"""
+	pass
