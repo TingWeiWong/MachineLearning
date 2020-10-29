581e4f06552ec3ac50bd67c9a9010f1cc017f168
Andrew <b06901160@ntu.edu.tw>
2020-10-29 23:04:38 +0800

    label_with_sign

--- StumpDecision/stump.py
+++ StumpDecision/stump.py
@@ -8,6 +8,28 @@ tau = 0
 # Placeholders
 Eout_minus_Ein = []
 
+
+# Label y from x using sign function
+def label_with_sign(x_list, data_set_size):
+	"""
+	This function applies the sign function on x_list,
+	which is drawn from uniform distribution
+	- Input:
+		* x_list: list of uniform distribution [-1,+1]
+		* data_size: size of x_list
+	- Returns:
+		* output_list: a same size list of x with y = sign(x)
+	"""
+	output_list = []
+
+	for i in range(data_size):
+		if x_list[i] > 0:
+			output_list.append(1)
+		else:
+			output_list.append(-1)
+
+	return output_list
+
 # Flipping Function
 def flipping(original_array, data_set_size, tau):
 	"""
