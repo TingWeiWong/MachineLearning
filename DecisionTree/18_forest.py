db70335ec1f3167400353f343a4fd05863dbe187
Andrew <andrew.wong.0101@gmail.com>
2021-01-17 02:05:42 +0800

    100 data

--- DecisionTree/18_forest.py
+++ DecisionTree/18_forest.py
@@ -9,51 +9,30 @@ forest = [0] * forest_number
 training_data = load_data("data/hw6_train.dat")
 testing_data = load_data("data/hw6_test.dat")
 
-contacted = [False] * 1000
+contacted = []
+for i in range(1000):
+	contacted.append([False] * forest_number)
 
 for i in range(forest_number):
 	x_random = []
 	for j in range(replacement):
 		randnum = random.randint(0,999)
-		contacted[randnum] = True
+		contacted[randnum][i] = True
 		x_random.append(training_data[randnum])
 	forest[i] = Construction(x_random)
 
-oob_data_list = []
-for i in range(1000):
-	if contacted[i] == False:
-		oob_data_list.append(training_data[i])
-
-if oob_data_list == []:
-	mode = 0
-else:
-	mode = 1
-
-print ("oob_data_list = ",oob_data_list)
-
-
-y_list = []
-for i in range(len(oob_data_list)):
-	temp = 0
-	for j in range(forest_number):
-		temp += get_label(Sort_data(oob_data_list[i], forest[j]))
-
-	if (temp >= 0):
-		y_list.append(1.0)
-	else:
-		y_list.append(-1.0)
-
+temp = 0
 error = 0
-if mode == 1:
-	for j in range(len(oob_data_list)):
-		if (oob_data_list[j][-1] != y_list[j]):
-			error += 1
-else:
-	print ("Empty")
+for i in range(len(training_data)):
+	for j in range(forest_number):
+		if contacted[i][j] == False:
+			temp += 1
+			if (training_data[i][-1] != get_label(Sort_data(testing_data[i], forest[j]))):
+				error += 1
+
+print (float(error) / temp)
 
 
-error = error / (1000)
-print ("error = ",error)
 
 
 
