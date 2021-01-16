f3774f715ec2de77adefbfdc93cbbe5778a35ceb
Andrew <andrew.wong.0101@gmail.com>
2021-01-17 01:24:15 +0800

    Using 20

--- DecisionTree/16_forest.py
+++ DecisionTree/16_forest.py
@@ -16,6 +16,30 @@ for i in range(forest_number):
 		x_random.append(training_data[randnum])
 	forest[i] = Construction(x_random)
 
+y_list = []
+for i in range(len(training_data)):
+	temp = 0
+	for j in range(forest_number):
+		temp += get_label(Sort_data(training_data[i], forest[j]))
+
+	if (temp >= 0):
+		y_list.append(1.0)
+	else:
+		y_list.append(-1.0)
+
+error = 0
+for j in range(len(training_data)):
+	if (training_data[j][-1] != y_list[j]):
+		error += 1
+
+error = error / (1000)
+print ("error = ",error)
+
+
+
+
+
+
 
 y_list = [0] * forest_number
 for i in range(forest_number):
